d7f5c35dfa6c21bb27f78cdb78e95c1a58debf04
RurikBTW <161582723+RurikBTW@users.noreply.github.com>
2024-05-31 14:08:38 +0300

    Delete lab3/PT-23-1/Konovalov Ivan Denisovich directory

--- lab3/PT-23-1/Konovalov Ivan Denisovich/open_zvit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-chcp 65001 > nul
-start /wait winword zvit.docx
--- lab3/PT-23-1/Konovalov Ivan Denisovich/part2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Шлях до каталогу, з якого потрібно видалити файли
-set "directory=C:\Users\09823\lab3\PT-23-1\Konovalov Ivan Denisovich"
-
-REM Видалення файлів з вказаного каталогу
-del /q "%directory%\*.*"
-
-pause
